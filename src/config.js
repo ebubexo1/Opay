028c62195fa7ac2800bb81b8b2dba331d0db5814
ebubexo1 <your-github-email@example.com>
2026-04-02 23:57:13 +0100

    update user config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,11 @@
 const config = {
-  userName: "CHIBUEZE",
+  userName: "PAUL",
   userPhone: "+234 812 345 6789",
-  userEmail: "chibueze@email.com",
+  userEmail: "ebube@gmail.com",
   accountNumber: "8012 3456 78",
   balance: "1,500,000.00",
   cashback: "2,450",
   points: "1,280",
   memberLevel: "Gold Member",
 }
-
 export default config
